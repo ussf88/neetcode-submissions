d67dcb0c1d630e51ad1874b5df425219aea6c67c
ussf88 <58928297+ussf88@users.noreply.github.com>
2026-09-12 22:54:30 +0100

    Add: top-k-elements-in-list - submission-26

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-26.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+      
+        int n = nums.Length;
+          Array.Sort(nums);
+        int lowestFreq = 0;
+        int lowestFreqNumber = nums[0];
+        int currentNumber = nums[0];
+        int currentFreq = 0;
+        var dict = new SortedDictionary<int, HashSet<int>>(
+    Comparer<int>.Create((x, y) => y.CompareTo(x))
+);
+      
+       
+        for(int i=0;i<n;i++){
+         if(nums[i]==currentNumber) currentFreq++;
+         else {
+         
+            if(dict.ContainsKey(currentFreq)) dict[currentFreq].Add(currentNumber);
+            else dict[currentFreq] = new HashSet<int>{currentNumber};
+            currentNumber=nums[i];
+            currentFreq = 1;
+
+         }
+         if(i==n-1){
+          
+            if(dict.ContainsKey(currentFreq)) dict[currentFreq].Add(currentNumber);
+            else dict[currentFreq] = new HashSet<int>{currentNumber};
+         }
+        }
+         
+        return dict.Values.SelectMany(e=>e).Take(k).ToArray();
+    }
+}

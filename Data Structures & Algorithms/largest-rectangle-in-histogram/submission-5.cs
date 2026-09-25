eaed7a97ca329710f2657d06d5addd2959fafd4e
ussf88 <58928297+ussf88@users.noreply.github.com>
2026-09-25 19:48:09 +0100

    Add: largest-rectangle-in-histogram - submission-5

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-5.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int LargestRectangleArea(int[] heights) {
+        int n = heights.Length;
+        Stack<int> stack = new();
+        Stack<int> stackleft = new();
+        var rightBoundries = new int [n];
+        var leftBoundries = new int [n];
+        for(int i=0;i<n;i++){
+            while(stack.TryPeek(out var topIndex) && heights[topIndex] > heights[i]){
+                stack.Pop();
+                rightBoundries[topIndex] = i-1;
+            }
+            stack.Push(i);
+        }
+        while(stack.Count>0){
+            var t = stack.Pop();
+            rightBoundries[t] =  n-1;
+        }
+        for(int i=n-1;i>=0;i--){
+            while(stackleft.Count>0 && heights[stackleft.Peek()]>heights[i]){
+                var t = stackleft.Pop();
+                leftBoundries[t] = i+1;
+            }
+            stackleft.Push(i);
+        }
+        int max = 0;
+        for(int i=0;i<n;i++){
+            int h = (rightBoundries[i] - leftBoundries[i] +1 )*heights[i];
+            max = Math.Max(h,max);
+        }
+    
+
+        
+        return max;
+    }
+}
